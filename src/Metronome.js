800395b5dea297b3d666e34153c133558405e90a
Irfan Chairurrachman <irfanchairurrachman@gmail.com>
2020-02-26 23:47:28 +0700

    the second but not final

---
+++ src/Metronome.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import click1 from './click1.wav';
+import click2 from './click2.wav';
+// import './Metronome.css';
+
+class Metronome extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playing: false,
+      count: 0,
+      bpm: 100,
+      beatsPerMeasure: 4
+    };
+
+    this.click1 = new Audio(click1);
+    this.click2 = new Audio(click2);
+  }
+
+  handleBpmChange = event => {
+    const bpm = event.target.value;
+    this.setState({bpm});
+  }
+
+  startStop = () => {
+    this.click1.play();
+   }
+ 
+
+  render() {
+    // let bpm = 100;
+    // let playing = false;
+    const {playing, bpm} = this.state;
+
+    return (
+      <div className="metronome">
+        <div className="bpm-slider">
+          <div>{bpm} BPM</div>
+          <input 
+            type="range" 
+            min="60" 
+            max="240" 
+            value={bpm}
+            onChange={this.handleBpmChange} />
+        </div>
+        <button onClick={this.startStop}>
+          {playing ? 'Stop' : 'Start'}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Metronome;
